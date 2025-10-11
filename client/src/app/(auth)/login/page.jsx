afa200e23aab21dbd46807a511525ed14e4a63ca
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-10-11 09:45:48 +0600

    login url updated

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
 
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/users/login',
+        'https://localhost:5000/api/users/login',
         formData,
         {
           headers: {
